5956d554038ed9b9832cf7d957867c2e36cb26c0
Nick <nickesc.gd@gmail.com>
2021-11-08 19:55:30 -0800

    try for vercel

--- server.js
+++ server.js
@@ -321,6 +321,7 @@ var port = 4000;
 //var server = app.listen(process.env.PORT || port);
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
+module.exports=app;
 app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
